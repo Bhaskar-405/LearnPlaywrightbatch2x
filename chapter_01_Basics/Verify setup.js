a9fee30ad773de0fab780b4b3b5bce992284e256
Bhaskar-405 <Bhaskar405rao@gmail.com>
2026-05-07 20:07:17 +0530

    Initial commit: Add Playwright basics chapter

---
+++ chapter_01_Basics/Verify setup.js
@@ -0,0 +1,8 @@
+//check the OS of system
+console.log(process.platform);
+//find architecture of system
+console.log(process.arch);
+//find version of nodejs
+console.log(process.version);
+//find current working directory
+console.log(process.cwd());
